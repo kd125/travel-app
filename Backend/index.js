1b16da566c8ae312847fc591e1cca9fe90b80585
Kd125 <kashdangar1250@gmail.com>
2023-08-27 17:28:40 -0500

    udpated the code to run on planetscale db vs local

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { parseISO, format, formatISO } = require("date-fns");
+const { parseISO, formatISO } = require("date-fns");
+const Knex = require("knex");
 
 require("dotenv").config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+// Configure Knex to use PlanetScale
+const knex = Knex({
+  client: "mysql2",
+  connection: process.env.DATABASE_URL,
+});
+
+console.log("Connected to PlanetScale!");
+// Create table if it doesn't exist
+knex.schema
+  .hasTable("travel_app")
+  .then(function (exists) {
+    if (!exists) {
+      return knex.schema.createTable("travel_app", function (table) {
+        table.increments("id").primary();
+        table.string("trip_name");
+        table.date("date");
+        table.string("location");
+        table.string("activity");
+        table.string("cost");
+        table.string("comments");
+      });
+    }
+  })
+  .then(() => console.log("Table created successfully"))
+  .catch((error) => console.log(`Error creating table: ${error}`));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -17,66 +41,54 @@ app.get("/", (req, res) => {
   res.send("This API is running!!!");
 });
 
-app.get("/api/get", (req, res) => {
-  const sqlSelect = "SELECT * FROM travelapp.travel_app;";
-  db.query(sqlSelect, (err, result) => {
+app.get("/api/get", async (req, res) => {
+  try {
+    const result = await knex.select("*").from("travel_app");
     res.send(result);
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching data: " + err.message);
+  }
 });
 
-app.get("/api/get/:id", (req, res) => {
+app.get("/api/get/:id", async (req, res) => {
   const tripId = req.params.id;
 
-  const sqlSelectId = "SELECT * FROM travelapp.travel_app WHERE id = ?";
-  db.query(sqlSelectId, [tripId], (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Error fetching trip by ID: ");
-    }
-
+  try {
+    const result = await knex("travel_app").where("id", tripId);
     res.send(result);
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching trip by ID: " + err.message);
+  }
 });
 
-app.post("/api/insert", (req, res) => {
-  const tripName = req.body.trip_name;
-  const tripDate = req.body.date;
-  const tripLocation = req.body.location;
-  const tripActivity = req.body.activity;
-  const tripCost = req.body.cost;
-  const tripComments = req.body.comments;
+app.post("/api/insert", async (req, res) => {
+  const { trip_name, date, location, activity, cost, comments } = req.body;
 
-  const date = tripDate ? tripDate : Date.now();
+  const formattedDate = date
+    ? formatISO(parseISO(date), { representation: "date" })
+    : formatISO(new Date(), { representation: "date" });
 
-  const formattedDate = formatISO(parseISO(date), {
-    representation: "date",
-  });
-
-  const sqlInsert =
-    "INSERT INTO travel_app (trip_name, date, location, activity, cost, comments) VALUES (?, ?, ?, ?, ?, ?)";
-  db.query(
-    sqlInsert,
-    [
-      tripName,
-      formattedDate,
-      tripLocation,
-      tripActivity,
-      tripCost,
-      tripComments,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res
-          .status(500)
-          .send("Error inserting data into the database: " + err.message);
-      } else {
-        res.status(200).send("Data inserted successfully.");
-      }
-    }
-  );
+  try {
+    await knex("travel_app").insert({
+      trip_name,
+      date: formattedDate,
+      location,
+      activity,
+      cost,
+      comments,
+    });
+    res.status(200).send("Data inserted successfully.");
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send("Error inserting data into the database: " + err.message);
+  }
 });
 
-app.listen(3001, () => {
-  console.log("Running on Port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
 });
